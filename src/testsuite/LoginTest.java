06877cd86d408a82d81da889f16de4f74ad1a008
PalakDevani <palak.devani85@gmail.com>
2022-11-21 14:13:08 +0000

    Project-3-sw-2-palakdevani

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,50 @@
+package testsuite;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utility.Utility;
+
+public class LoginTest extends Utility {
+
+    String baseUrl = "http://the-internet.herokuapp.com/login";
+
+    @Before
+    public void setBaseUrl() {
+        openBrowser(baseUrl);
+    }
+
+    @Test
+    public void userShouldLoginSuccessfullyWithValidCredentials() {
+        sendTextToElement(By.id("username"), "tomsmith");
+        sendTextToElement(By.name("password"), "SuperSecretPassword!");
+        clickOnElement(By.xpath("//button[@class='radius']"));
+        String expectedText = "Secure Area";
+        String actualText = getTextFromElement(By.xpath("//div[@id=\"content\"]/div/h2"));
+        Assert.assertEquals(expectedText, actualText);
+    }
+
+    @Test
+    public void verifyTheUsernameErrorMessage() {
+        sendTextToElement(By.id("username"), "tomsmith1");
+        sendTextToElement(By.name("password"), "SuperSecretPassword!");
+        clickOnElement(By.xpath("//button[@class='radius']"));
+        String expectedMessage = "Your username is invalid!\n" + "×";
+        String actualMessage = getTextFromElement(By.xpath("//div[@id='flash-messages']/div[1]"));
+        Assert.assertEquals(expectedMessage, actualMessage);
+
+    }
+
+    @Test
+    public void verifyThePasswordErrorMessage() {
+        sendTextToElement(By.id("username"), "tomsmith");
+        sendTextToElement(By.name("password"), "SuperSecretPassword");
+        clickOnElement(By.xpath("//button[@class='radius']"));
+        String expectedMessage = "Your password is invalid!\n" + "×";
+        String actualMessage = getTextFromElement(By.xpath("//div[@id='flash-messages']//div[1]"));
+        Assert.assertEquals(expectedMessage, actualMessage);
+    }
+
+
+}
